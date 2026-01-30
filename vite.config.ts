052af9d9ade25fa275901691ce0bcaec6742f838
Mirko Sandonà <favouriteuser@tiscali.it>
2026-01-30 13:00:39 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: '/png2ico/',
+  base: '/',
   plugins: [
     react(),
     mode === 'development' && componentTagger(),
